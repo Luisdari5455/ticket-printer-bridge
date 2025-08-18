d2eaacb83bcf7d7441dfb0cff9b6e8e31f0baa80
luisCWotdev <luis.cotoc@wotdev.com>
2025-08-17 21:57:34 -0600

    change to prod

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 function env(k, def) { return process.env[k] ?? def; }
 module.exports = {
-  BACKEND_WS: env("BACKEND_WS", "http://localhost:3001"),
+  BACKEND_WS: env("BACKEND_WS", "https://ticketapi-ceqz.onrender.com"),
   LOCATION_ID: env("LOCATION_ID", "sucursal-central-01"),
   BRIDGE_TOKEN: env("BRIDGE_TOKEN", "changeme"),
   PRINTER_ENCODING: env("PRINTER_ENCODING", "GB18030"),
